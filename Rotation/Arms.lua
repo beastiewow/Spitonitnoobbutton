6a9dab259da3aa1474348e18cb2bf2615526f7b6
beastiewow <121821585+meekel4d@users.noreply.github.com>
2025-01-08 12:25:50 -0500

    Memory fix
    Removed debug prints, adjusted slam window due to new throttle function

--- Rotation/Arms.lua
+++ Rotation/Arms.lua
@@ -24,45 +24,37 @@ function SNB.ArmsSingleTargetUnified()
     -- Overpower logic
     if SNB.isOverpowerMode and overpowerReady and GetTime() < (SNB.overpowerProcEndTime - 0.6) and st_timer < 2 and msCooldown >= 0.5 then
         if SNB.IsInBattleStance() and rage > 5 then
-            SNB.debug_print("Casting Overpower due to proc, sufficient time left, and rage > 5")
             CastSpellByName("Overpower")
             return
         elseif not SNB.IsInBattleStance() then
-            SNB.debug_print("Switching to Battle Stance for Overpower")
             CastSpellByName("Battle Stance")
             return
         end
     elseif not overpowerReady and not SNB.IsInBerserkerStance() then
-        SNB.debug_print("Switching to Berserker Stance as Overpower is not ready")
         CastSpellByName("Berserker Stance")
     end
 
     -- Mortal Strike logic
     if rage >= 30 and msCooldown <= 0 then
-        SNB.debug_print("Casting Mortal Strike")
         CastSpellByName("Mortal Strike")
         return
     end
 
     -- Whirlwind logic based on toggle
-    if SNB.isWhirlwindMode and rage >= 25 and wwCooldown <= 0 and msCooldown > 1.5 then
-        SNB.debug_print("Casting Whirlwind")
+    if SNB.isWhirlwindMode and rage >= 25 and wwCooldown <= 0 and msCooldown > 1.2 then
         CastSpellByName("Whirlwind")
         return
     end
 
     -- Slam logic
     if rage >= 30 and (st_timer < 3.4 and st_timer > 1.4) then
-        SNB.debug_print("Casting Slam due to high rage and cooldown proximity")
         CastSpellByName("Slam")
     end
 
     -- Heroic Strike logic
     if not SNB.IsHeroicStrikeQueued() and rage > 100 and st_timer > 3 then
-        SNB.debug_print("Queuing Heroic Strike")
         CastSpellByName("Heroic Strike")
     elseif SNB.IsHeroicStrikeQueued() and rage < 60 then
-        SNB.debug_print("Canceling Heroic Strike due to low rage")
         SpellStopCasting("Heroic Strike")
     end
 end
@@ -96,7 +88,7 @@ function SNB.ArmsAOERotation()
         CastSpellByName("Whirlwind")
     elseif rage >= 70 and msCooldown <= 0 and wwCooldown > 1.5 then
         CastSpellByName("Mortal Strike")
-    elseif rage >= 30 and wwCooldown >=0.2 and st_timer > 1.5 then
+    elseif rage >= 30 and wwCooldown >=0.2 and st_timer > 1.2 then
         -- Condition 1: High rage and both Mortal Strike and Whirlwind are close to coming off cooldown
         CastSpellByName("Slam")
     end
@@ -105,10 +97,8 @@ function SNB.ArmsAOERotation()
     if not SNB.IsCleaveQueued() then
         -- 1. Cancel if both MS and WW cooldowns are <= 1.5, less than 70 rage, and swing timer <= 0.5
         if wwCooldown > 3.5 and rage > 90 and st_timer > 0.5 then
-            SNB.debug_print("Canceling Cleave due to incoming MS and low rage")
             CastSpellByName("Cleave")
         elseif rage > 70 and st_timer > 0.5 and wwCooldown > 5.5 then
-            SNB.debug_print("Canceling Cleave due to low rage")
             CastSpellByName("Cleave")
         end
     end
@@ -118,10 +108,8 @@ function SNB.ArmsAOERotation()
     if SNB.IsCleaveQueued() then
         -- 1. Cancel if both MS and WW cooldowns are <= 1.5, less than 70 rage, and swing timer <= 0.5
         if wwCooldown < 1.5 and rage < 35 and st_timer < 0.5 then
-            SNB.debug_print("Canceling Cleave due to incoming WW and low rage")
             SpellStopCasting("Cleave")
         elseif rage < 20 and st_timer < 0.5 then
-            SNB.debug_print("Canceling Cleave due to low rage")
             SpellStopCasting("Cleave")
         end
     end
@@ -152,7 +140,6 @@ function SNB.CastSweepingStrikes()
     -- If Sweeping Strikes is on cooldown or already active, switch to Berserker Stance
     if ssCooldown > 0 or isSweepingStrikesActive then
         if not SNB.IsInBerserkerStance() then
-            SNB.debug_print("Switching to Berserker Stance because Sweeping Strikes is unavailable.")
             CastSpellByName("Berserker Stance")
         end
         return
@@ -160,23 +147,19 @@ function SNB.CastSweepingStrikes()
 
     -- If Sweeping Strikes is off cooldown and not active, ensure enough rage
     if rage < 10 and brCooldown <= 0 then
-        SNB.debug_print("Not enough rage for Sweeping Strikes. Using Bloodrage.")
         CastSpellByName("Bloodrage")
         return -- Exit to let Bloodrage generate rage before continuing
     end
 
     -- Switch to Battle Stance and cast Sweeping Strikes
     if not SNB.IsInBattleStance() then
-        SNB.debug_print("Switching to Battle Stance for Sweeping Strikes.")
         CastSpellByName("Battle Stance")
     end
 
-    SNB.debug_print("Casting Sweeping Strikes.")
     CastSpellByName("Sweeping Strikes")
 
     -- After casting Sweeping Strikes, swap back to Berserker Stance
     if not SNB.IsInBerserkerStance() then
-        SNB.debug_print("Switching back to Berserker Stance after Sweeping Strikes.")
         CastSpellByName("Berserker Stance")
     end
 end
